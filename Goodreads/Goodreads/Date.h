ea68dc6bfae4434fff42264ca5921534222a2f2c
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2026-05-16 10:03:32 +0300

    Add User base class implementation saved
    Added User base class declaration saved

---
+++ Goodreads/Goodreads/Date.h
@@ -0,0 +1,8 @@
+#pragma once
+class Date
+{
+	int day = 0;
+	int month = 0;
+	int year = 0;
+};
+
---
+++ Goodreads/Goodreads/User.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Date.h"
+#include "Message.h"
+class User
+{
+private:
+	std::string username;
+	std::string password;
+	Date registrationDate;
+	std::vector<std::string> followers;
+	std::vector<Message> inbox;
+public:
+	User() = default;
+	User(const std::string& username, const std::string& password, const Date& registrationDate);
+	virtual ~User() = default;
+	bool checkPassword(const std::string& password) const;
+	void addFollower(const std::string& follower);
+	bool hasFollower(const std::string& follower) const;
+	void receiveMessage(const Message& message);
+
+	const std::string& getUsername() const;
+	const std::string& getPassword() const;
+	const Date& getRegistrationDate() const;
+	const std::vector<std::string>& getFollowers() const;
+	const std::vector<Message>& getInbox() const;
+	std::vector<Message>& getInbox();
+
+	void setUsername(const std::string& username);
+	void setPassword(const std::string& password);
+	void setRegistrationDate(const Date& date);
+
+	static void validateUsername(const std::string& username);
+	static void validatePassword(const std::string& password);
+	static std::string passwordCoded(const std::string& password);
+};
+
