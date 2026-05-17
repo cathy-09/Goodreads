2e60fb949d7d476163267c1cf85c89107e13b730
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2026-05-17 13:49:25 +0300

    Add Author class implementation
    Implemented Author class extending Reader:
    - published books
    - publisher logic
    - add and remove publisher functionality
    - override Author role

---
+++ Goodreads/Goodreads/Author.cpp
@@ -0,0 +1,51 @@
+#include "Author.h"
+Author::Author(const std::string& username, const std::string& password, const Date& registrationDate)
+	:Reader(username, password, registrationDate)
+{
+
+}
+
+UserType Author::type() const
+{
+	return UserType::Author;
+}
+
+void Author::addPublishedBook(const std::string& title)
+{
+	if (std::find(publishedBooks.begin(), publishedBooks.end(), title) == publishedBooks.end())
+	{
+		publishedBooks.push_back(title);
+	}
+}
+
+bool Author::worksWithPublisher(const std::string& publisher) const
+{
+	return std::find(publishers.begin(), publishers.end(), publisher) != publishers.end();
+}
+
+void Author::addPublisher(const std::string& publisher)
+{
+	if (!worksWithPublisher(publisher))
+	{
+		publishers.push_back(publisher);
+	}
+}
+
+void Author::removePublisher(const std::string& publisher)
+{
+	auto publisherToRemove = std::find(publishers.begin(), publishers.end(), publisher);
+	if (publisherToRemove != publishers.end())
+	{
+		publishers.erase(publisherToRemove);
+	}
+}
+
+const std::vector<std::string>& Author::getPublishedBooks() const
+{
+	return publishedBooks;
+}
+
+const std::vector<std::string>& Author::getPublishers() const
+{
+	return publishers;
+}
---
+++ Goodreads/Goodreads/Author.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "User.h"
+#include "Reader.h"
+
+class Author : public Reader
+{
+private:
+	std::vector<std::string> publishedBooks;
+	std::vector<std::string> publishers;
+public:
+	Author() = default;
+	Author(const std::string& username, const std::string& password, const Date& registrationDate);
+	UserType type() const override;
+	void addPublishedBook(const std::string& title);
+	bool worksWithPublisher(const std::string& publisher) const;
+	void addPublisher(const std::string& publisher);
+	void removePublisher(const std::string& publisher);
+
+
+	const std::vector<std::string>& getPublishedBooks() const;
+	const std::vector<std::string>& getPublishers() const;
+};
+
