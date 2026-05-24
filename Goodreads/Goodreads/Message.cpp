2bdead49db2137c2cb697218e391a6355d81657a
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2026-05-24 09:26:10 +0300

    Implement Message class
    - Implemented isJobOffer()
    - Implemented isFollowNotice()
    - Implemented isBookNotice()
    - Implemented isLikedNotice()
    - Implemented isFriendNotice()

--- Goodreads/Goodreads/Message.cpp
+++ Goodreads/Goodreads/Message.cpp
@@ -1,1 +1,26 @@
 #include "Message.h"
+
+bool Message::isJobOffer() const
+{
+	return type == MessageType::JobOffer;
+}
+
+bool Message::isFollowNotice() const
+{
+	return type == MessageType::FollowNotice;
+}
+
+bool Message::isBookNotice() const
+{
+	return type == MessageType::BookNotice;
+}
+
+bool Message::isLikedNotice() const
+{
+	return type == MessageType::FollowNotice;
+}
+
+bool Message::isFriendNotice() const
+{
+	return type == MessageType::FriendNotice;
+}
--- Goodreads/Goodreads/Message.h
+++ Goodreads/Goodreads/Message.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+
+enum class MessageType
+{
+    Regular,
+    LikedNotice,
+    FollowNotice,
+    FriendNotice,
+    BookNotice,
+    JobOffer 
+};
+
 class Message
 {
+private:
+    std::string from;
+    std::string content;
+    MessageType type = MessageType::Regular;
+    bool isRead = false;
+public:
+    bool isJobOffer() const;
+    bool isFollowNotice() const; 
+    bool isBookNotice() const;
+    bool isLikedNotice() const;
+    bool isFriendNotice() const;
 };
 
